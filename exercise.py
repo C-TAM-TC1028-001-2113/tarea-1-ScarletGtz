fd98d2869a10497cf66cbb9125344729a047ef84
ScarletGtz <89593734+ScarletGtz@users.noreply.github.com>
2021-08-29 22:54:29 -0500

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,13 @@
+def main():
+    x1 = float(input("Dame x1:"))
+    y1 = float(input("Dame y1:"))
+    x2 = float(input("Dame x2:"))
+    y2 = float(input("Dame y2:"))
+    m = (y2 - y1) / (x2 - x1)
+    print ("Su pendiente es:", m)
+
+
+
+
+if __name__ == '__main__':
+    main()
