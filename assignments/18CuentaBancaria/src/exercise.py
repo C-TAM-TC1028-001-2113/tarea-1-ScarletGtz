a5e7c96a559431cc5a8df93c2dc94b9a8f3cda98
scarl <a01411920@itesm.mx>
2021-08-29 23:32:33 -0500

    cheque

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -2,9 +2,9 @@ def main():
     smes = float(input("Dame el saldo del mes anterior:"))
     ing = float(input("Dame los ingresos:"))
     egr = float(input("Dame los egresos:"))
-    nchec = int(input("Dame el numero de cheques:"))
+    nchec = int(input("Dame el número de cheques:"))
     saldo = ( smes + ing - egr - (nchec * 13) ) * (1 - 0.075)
-    print("El saldo final de la cuenta es:$", saldo)
+    print("El saldo final de la cuenta es:", saldo)
 
 if __name__ == '__main__':
     main()
