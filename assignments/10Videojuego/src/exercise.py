51ac0ca2a7be8cd62923adfa0df8ec859e445aaf
scarl <a01411920@itesm.mx>
2021-08-29 23:30:59 -0500

    videojuego

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -2,7 +2,7 @@ def main():
     nuev = int(input("Dame la cantidad de juegos nuevos:"))
     usad = int(input("Dame la cantidad de juegos usados:"))
     t = (nuev * 1000) + (usad * 350)
-    print("El total de la compra es:$", t)
+    print("El total de la compra es:", t)
 
 
 if __name__ == '__main__':
